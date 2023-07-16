1ad357907ad98dc5fb9636136ce2e38db0872b60
Harshita Bhatia <85699780+HarshitaBhatia27@users.noreply.github.com>
2023-07-16 17:02:13 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="db",
+    version="0.0.1",
+    author="Harshita",
+    author_email="harshitabhatia272002@gmal.com",
+    packages=find_packages(),
+    install_requires=[],
+)
